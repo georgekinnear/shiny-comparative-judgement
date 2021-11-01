88dd41d1914c9ffa6fe0d615c6373da1dec6563c
George Kinnear <G.Kinnear@ed.ac.uk>
2021-11-01 15:34:29 +0000

    outsource the CJ functions to an external script

--- app.R
+++ app.R
@@ -75,7 +75,6 @@ ui <- fluidPage(
   ),
   
   # Placeholder for page content - the server will update this as needed
-  uiOutput("pageContent")
   uiOutput("pageContent"),
   uiOutput("debugging")
 )
@@ -83,6 +82,10 @@ ui <- fluidPage(
 
 
 server <- function(input, output, session) {
+  
+  # This file contains helper functions that implement the approach to choosing pairs/tuples to compare
+  # (local = TRUE is needed to ensure the functions have the right scope, e.g. access to the pool database connection)
+  source("cj_functions.R", local = TRUE)
   
   # These will be global variables within each session
   assigned_study <- NULL
@@ -226,8 +229,7 @@ server <- function(input, output, session) {
       )
     )
   })
- 
-  
+
   #
   # Page 1 - judging instructions
   #
@@ -292,172 +294,6 @@ server <- function(input, output, session) {
   # Judging
   #
   
-  make_pairs <- function(pairs_to_make = 20) {
-    
-    # 1. Gather data on which judgements have been made already in this study group
-
-    # all comparisons from this study
-    judgement_data <- pool %>% 
-      tbl("judgements") %>% 
-      filter(study == !!session_info$study_id) %>% 
-      select(-contains("comment")) %>% 
-      collect() %>% 
-      mutate(across(c("left", "right"), as.integer))
-
-    # count the number of comparisons for each pair
-    pairs_judged <- judgement_data %>%
-      rowwise() %>%
-      mutate(pair = paste(sort(c(left, right)), collapse = '_')) %>% 
-      select(pair, judge_id) %>% 
-      group_by(pair) %>% 
-      tally() %>% 
-      arrange(-n) %>% 
-      separate(pair, c("s1", "s2"), "_") %>% 
-      mutate(across(c("s1", "s2"), as.integer))
-    
-    # systematically list all pairs, and add the counts for each
-    # (filter for item_num > 0 to exclude the attention check item at this point)
-    all_pairs_status <-
-      crossing(scripts %>% filter(item_num > 0) %>% select(s1 = item_num),
-               scripts %>% filter(item_num > 0) %>% select(s2 = item_num)) %>%
-      filter(s1 < s2) %>%
-      left_join(pairs_judged, by = c("s1", "s2")) %>%
-      mutate(n = replace_na(n, 0))
-    
-    # 2. Select pairs from among the least judged so far
-    
-    pairs_to_judge <- tibble()
-    
-    while(nrow(pairs_to_judge) < pairs_to_make) {
-      
-      if(nrow(pairs_to_judge) == 0) {
-        # start with the least judged pairs
-        new_pairs_to_judge <- all_pairs_status %>% 
-          filter(n == min(n)) %>% 
-          slice_sample(n = pairs_to_make)
-      } else {
-        # but if we have selected some pairs already, remove them from
-        # consideration (using anti_join) and look at the next-least-judged pairs
-        new_pairs_to_judge <- all_pairs_status %>% 
-          anti_join(pairs_to_judge, by = c("s1", "s2", "n")) %>% 
-          filter(n == min(n)) %>% 
-          slice_sample(n = pairs_to_make)
-      }
-      
-      pairs_to_judge <- bind_rows(pairs_to_judge,
-                                  new_pairs_to_judge)
-    }
-    
-    return(pairs_to_judge %>%
-             # trim to the desired number of pairs
-             slice_head(n = pairs_to_make) %>% 
-             # shuffle the scripts into left and right
-             mutate(
-               x = sample(c(0,1), size = pairs_to_make, replace = TRUE),
-               left = ifelse(x==0, s1, s2),
-               right = ifelse(x==0, s2, s1)
-             ) %>% 
-             select(left, right) %>%
-             mutate(pair_num = row_number(), .before = 1)
-    )
-  }
-  next_pair = function(old_pair_num) {
-
-    # move on to the next pair
-    pair_to_return = old_pair_num + 1
-    
-    # if we've reached the end, add 20 more pairs to the list
-    if(pair_to_return > nrow(pairs)) {
-      pairs <<- pairs %>% bind_rows(make_pairs(pairs_to_make = 20) %>% mutate(pair_num = pair_num + old_pair_num))
-      pair$pairs_available <- nrow(pairs)
-      # print(pairs)
-    }
-    pairs %>% 
-      filter(pair_num == pair_to_return) %>%
-      head(1)
-  }
-  
-  make_tuple <- function() {
-    
-    # 1. Gather data on which judgements have been made already in this study group
-    
-    # all comparisons from this study
-    judgement_data <- pool %>% 
-      tbl("rankings") %>% 
-      #filter(study == !!session_info$study_id) %>% 
-      select(ranking_id, items_presented) %>% 
-      collect() %>% 
-      separate_rows(items_presented, sep = ",")
-    
-    # count the number of comparisons for each pair
-    pairs_judged <- judgement_data %>% 
-      left_join(judgement_data, by = "ranking_id") %>% 
-      rename(left = items_presented.x, right = items_presented.y) %>% 
-      # strip out the spurious self-comparisons
-      filter(left != right) %>%
-      # keep only the pairs in order e.g. 1_2 not 2_1
-      rowwise() %>% 
-      # put the pairs in order, with item IDs as integers
-      mutate(across(c("left", "right"), as.integer)) %>% 
-      mutate(pair = paste(sort(c(left, right)), collapse = '_')) %>% 
-      select(pair, ranking_id) %>% 
-      distinct() %>% 
-      group_by(pair) %>% 
-      tally() %>% 
-      arrange(-n) %>% 
-      separate(pair, c("s1", "s2"), "_") %>% 
-      mutate(across(c("s1", "s2"), as.integer))
-    
-    # systematically list all pairs, and add the counts for each
-    # (filter for item_num > 0 to exclude the attention check item at this point)
-    all_pairs_status <-
-      crossing(scripts %>% filter(item_num > 0) %>% select(s1 = item_num),
-               scripts %>% filter(item_num > 0) %>% select(s2 = item_num)) %>%
-      filter(s1 < s2) %>%
-      left_join(pairs_judged, by = c("s1", "s2")) %>%
-      mutate(n = replace_na(n, 0))
-    
-    # 2. Select tuples that include pairs from among the least judged so far
-    
-    # (i) pick one of the least judged pairs, (A,B)
-    base_pair = all_pairs_status %>% 
-      filter(n == min(n)) %>% 
-      slice_sample(n = 1)
-    A <- base_pair["s1"] %>% deframe()
-    B <- base_pair["s2"] %>% deframe()
-    
-    # (ii) pick novel pairs involving A and B that are among the least judged
-    C <- all_pairs_status %>% 
-      filter(s1 == A | s2 == A) %>% 
-      filter(s1 != B, s2 != B) %>% 
-      filter(n == min(n)) %>% 
-      slice_sample(n = 1) %>% 
-      mutate(c = if_else(s1 == A, s2, s1)) %>% 
-      select(c) %>% 
-      deframe()
-    D <- all_pairs_status %>% 
-      filter(s1 == B | s2 == B) %>% 
-      filter(s1 != A, s2 != A, s1 != C, s2 != C) %>% 
-      filter(n == min(n)) %>% 
-      slice_sample(n = 1) %>% 
-      mutate(d = if_else(s1 == B, s2, s1)) %>% 
-      select(d) %>% 
-      deframe()
-    
-    # (iii) pick a further novel pair, being one of the least judged pairs that have
-    #       one of A,B,C,D paired with another item
-    E <- all_pairs_status %>% 
-      filter((s1 %in% c(A,B,C,D) & !s2 %in% c(A,B,C,D)) | (!s1 %in% c(A,B,C,D) & s2 %in% c(A,B,C,D))) %>% 
-      filter(n == min(n)) %>% 
-      slice_sample(n = 1) %>% 
-      mutate(e = if_else(s1 %in% c(A,B,C,D), s2, s1)) %>% 
-      select(e) %>% 
-      deframe()
-    
-    return(c(A,B,C,D,E))
-    
-  }
-  
   # initialise empty data structures, to be used when judging begins
   pair <- reactiveValues(
     pair_num = 0,
@@ -467,7 +303,6 @@ server <- function(input, output, session) {
   )
   pairs <- tibble()
   
-  observeEvent(input$startComparing, {
   # 
   #  Demo of the sortable interface
   # 
